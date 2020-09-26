d47b9d20adbdf6ccc263a35098a2042380f38011
Moringa School <dorcasmosonik@gmail.com>
2020-09-26 15:58:50 +0300

    generate class

---
+++ src/app/quote.ts
@@ -0,0 +1,13 @@
+export class Quote {
+    constructor(
+        public id: number,
+        public quote: string,
+        public author: string,
+        public user: string,
+        public createdAt: Date,
+        public upvotes: number,
+        public downvotes: number) {
+    }
+}
+
+
