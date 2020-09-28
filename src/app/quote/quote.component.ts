cc15bcd735a15d2502b7374f40665d601997db1f
Moringa School <dorcasmosonik@gmail.com>
2020-09-28 22:22:42 +0300

    date pipe

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -11,10 +11,10 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   mQuotes: Quote[] = [
-    new Quote(0, "If you would convince a man that he does wrong, do right. But do not care to convince him. Men will believe what they see. Let them see.", "Henry David Thoreau", "Dorcas Mosonik", new Date(), 0, 0),
-    new Quote(0, "Hide yourself in God, so when a man wants to find you he will have to go there first.", "Shannon L. Alder", "Clinton Thomas", new Date(), 0, 0),
-    new Quote(0, "I would rather be an artist than a leader. Ironically, a leader has to follow the rules.", "Criss Jami, Venus in Arms", "Rowena Rono", new Date(), 0, 0),
-    new Quote(0, "To bring up a child in the way he should go, travel that way yourself once in a while.", "Josh Billings", "Ronald Kiprotich", new Date(), 0, 0),
+    new Quote(0, "If you would convince a man that he does wrong, do right. But do not care to convince him. Men will believe what they see. Let them see.", "Henry David Thoreau", "Dorcas Mosonik", new Date(2020,9,8), 0, 0),
+    new Quote(1, "Hide yourself in God, so when a man wants to find you he will have to go there first.", "Shannon L. Alder", "Clinton Thomas", new Date(2020,9,10), 0, 0),
+    new Quote(2, "I would rather be an artist than a leader. Ironically, a leader has to follow the rules.", "Criss Jami, Venus in Arms", "Rowena Rono", new Date(2020,8,30), 0, 0),
+    new Quote(3, "To bring up a child in the way he should go, travel that way yourself once in a while.", "Josh Billings", "Ronald Kiprotich", new Date(2020,9,19), 0, 0),
 
   ];
 
